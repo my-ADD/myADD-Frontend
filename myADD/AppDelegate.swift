058b282ddbb57a3403611476be0187c3dcd1187f
s01ver <solverglace@gmail.com>
2023-07-03 02:17:04 +0900

    ignore & workspace

--- myADD/AppDelegate.swift
+++ myADD/AppDelegate.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 import CoreData
+import UIKit
+import CoreData
+import KakaoSDKCommon
+import KakaoSDKTalk
+import KakaoSDKAuth
+import KakaoSDKUser
+import KakaoSDKTemplate
+import KakaoSDKShare
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +24,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        instance?.isNaverAppOauthEnable = true
+        instance?.isInAppOauthEnable = true
+        instance?.isOnlyPortraitSupportedInIphone()
+        
+        instance?.serviceUrlScheme = kServiceAppUrlScheme
+        instance?.consumerKey = kConsumerKey
+        instance?.consumerSecret = kConsumerSecret
+        instance?.appName = kServiceAppName
+
+        KakaoSDK.initSDK(appKey:"")
+                    
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+           
+        NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+        
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                return AuthController.handleOpenUrl(url: url)
+            }
+
+            return false
     }
 
     // MARK: UISceneSession Lifecycle
